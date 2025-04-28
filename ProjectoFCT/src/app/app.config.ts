26eab957805eb102b018cf7a8a2bdef915431e8f
vcerdan <v.cerdan@pro2fp.es>
2025-04-28 10:40:57 +0200

    Se instalo iconic ya que es recomendable para el uso de aplicaciones moviles y configuracion del fichero app.config.ts para poder asi empezar con la creacion de la aplicacion

--- ProjectoFCT/src/app/app.config.ts
+++ ProjectoFCT/src/app/app.config.ts
@@ -1,9 +1,20 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
 
+import { importProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+import { IonicModule } from '@ionic/angular';
+
+
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    importProvidersFrom(IonicModule.forRoot()),
+    provideRouter(routes),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideClientHydration(withEventReplay())
+  ]
 };
+
